6898ecaeefc6beecaee66e4794318d40a8d43ecd
Jose Zamora <zamoraz@hotmail.com>
2023-04-09 18:19:36 -0400

    Nueva Conexion

--- src/main/java/conexion/ConexionSingleton.java
+++ src/main/java/conexion/ConexionSingleton.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConexionSingleton {
     private static ConexionSingleton instance = new ConexionSingleton();
-    private static final String URL = "jdbc:mysql://localhost:3306/m5_abpro_5";
+    private static final String URL = "jdbc:mysql://localhost:3306/sprint_5";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String USER = "root";
     private static final String PASSWORD = "12345";
