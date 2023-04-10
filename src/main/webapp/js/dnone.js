c67e01e77f999a26a2bc7c3d2d1cb9f51db38301
AndresSCP <andres.contreras.p@gmail.com>
2023-04-09 23:18:18 -0400

    agrega dnone nuevamente

---
+++ src/main/webapp/js/dnone.js
@@ -0,0 +1,23 @@
+function displayFormFields() {
+    var selectedOption = document.getElementById("tipoUsuario").value;
+    if (selectedOption == "cliente") {
+        document.getElementById("formulario-cliente").classList.remove("d-none");
+        document.getElementById("formulario-profesional").classList.add("d-none");
+        document.getElementById("formulario-administrativo").classList.add("d-none");
+        document.getElementById("formulario-usuario").classList.add("d-none");
+    } else if (selectedOption == "profesional") {
+        document.getElementById("formulario-cliente").classList.add("d-none");
+        document.getElementById("formulario-profesional").classList.remove("d-none");
+        document.getElementById("formulario-administrativo").classList.add("d-none");
+        document.getElementById("formulario-usuario").classList.add("d-none");
+    } else if (selectedOption == "administrativo") {
+        document.getElementById("formulario-cliente").classList.add("d-none");
+        document.getElementById("formulario-profesional").classList.add("d-none");
+        document.getElementById("formulario-administrativo").classList.remove("d-none");
+        document.getElementById("formulario-usuario").classList.add("d-none");
+    } else if (selectedOption == "usuario") {
+        document.getElementById("formulario-cliente").classList.add("d-none");
+        document.getElementById("formulario-profesional").classList.add("d-none");
+        document.getElementById("formulario-administrativo").classList.add("d-none");
+        document.getElementById("formulario-usuario").classList.remove("d-none");
+}
